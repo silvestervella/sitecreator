9a9bd051a0bac22a355cf3dbf50519e75c507bab
Silvester Vella <silvestervella@Silvesters-iMac.lan>
2019-10-02 10:36:46 +0200

    hmdgmg

--- app/wp-content/themes/heythere-stable-child/functions.php
+++ app/wp-content/themes/heythere-stable-child/functions.php
@@ -113,5 +113,60 @@ function sitecreator_wpf_dev_show_fields_options_setting() {
 add_action( 'wpforms_fields_show_options_setting', 'sitecreator_wpf_dev_show_fields_options_setting', 10, 2 );
 
 
+/**
+ * 3. Add page attributes to posts
+*/
 add_post_type_support( 'post', 'page-attributes' );
+
+
+
+
+
+/**
+ * 4. Woocommerce show variations
+*/
+/**
+ * Replace add to cart button in the loop.
+ */
+function iconic_change_loop_add_to_cart() {
+  remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+  add_action( 'woocommerce_after_shop_loop_item', 'iconic_template_loop_add_to_cart', 10 );
+}
+
+add_action( 'init', 'iconic_change_loop_add_to_cart', 10 );
+
+/**
+* Use single add to cart button for variable products.
+*/
+function iconic_template_loop_add_to_cart() {
+  global $product;
+
+  if ( ! $product->is_type( 'variable' ) ) {
+      woocommerce_template_loop_add_to_cart();
+      return;
+  }
+
+  remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
+  add_action( 'woocommerce_single_variation', 'iconic_loop_variation_add_to_cart_button', 20 );
+
+  woocommerce_template_single_add_to_cart();
+}
+
+/**
+* Customise variable add to cart button for loop.
+*
+* Remove qty selector and simplify.
+*/
+function iconic_loop_variation_add_to_cart_button() {
+  global $product;
+
+  ?>
+  <div class="woocommerce-variation-add-to-cart variations_button">
+      <button type="submit" class="single_add_to_cart_button button"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+      <input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
+      <input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
+      <input type="hidden" name="variation_id" class="variation_id" value="0" />
+  </div>
+  <?php
+}
 ?>
