2b088bd922a88bb3d0e0c8f35f859d9ca6669bf4
Silvester Vella <silvestervella@Silvesters-iMac.lan>
2019-10-07 12:55:50 +0200

    hgdtjuy

--- app/wp-content/plugins/sitecreator-envatoThemes/index.php
+++ app/wp-content/plugins/sitecreator-envatoThemes/index.php
@@ -27,53 +27,41 @@ function sitecreator_envato_init(){
     sitecreator_get_envato_themes();
   }
 
+    // Check whether the button has been pressed AND also check the nonce
+    if (isset($_POST['envatothemes_getcollections']) && check_admin_referer('envatothemes_getcollections_clicked')) {
+      // the button has been pressed AND we've passed the security check
+      sitecreator_get_envato_collections();
+    }
+
   echo '<form action="'. admin_url("admin.php?page=get-envato-plugin") .'" method="post">';
 
   // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('envatothemes_button_clicked');
   echo '<input type="hidden" value="true" name="envatothemes_button" />';
+  echo 'Get themes';
+  submit_button('Call Function');
+  echo '</form>';
+  echo '</div>';
+
+
+  echo '<form action="'. admin_url("admin.php?page=get-envato-plugin") .'" method="post">';
+
+  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
+  wp_nonce_field('envatothemes_getcollections_clicked');
+  echo '<input type="hidden" value="true" name="envatothemes_getcollections" />';
+  echo 'Get collections';
   submit_button('Call Function');
   echo '</form>';
   echo '</div>';
 
 }
 
-// function envatothemes_button_action()
-// {
-//   echo '<div id="message" class="updated fade"><p>'
-//     .'The "Call Function" button was clicked.' . '</p></div>';
 
-//   //$path = WP_TEMP_DIR . '/envatothemes-button-log.txt';
-
-//   //$handle = fopen($path,"w");
-
-//   if ($handle == false) {
-//     echo '<p>Nuffin done nigga!</p>';
-//   }
-//   else {
-//     echo '<p>Themes Added.</p>';
-
-//   }
-// }
-
-
-
-// // check if transient has expired
-// function sitecreator_check_transient() {
-//   if(($envato_themes = get_transient("get_envato_transient")) === false) 
-//     { 
-//       $envato_themes = sitecreator_get_envato_themes();
-//       set_transient("get_envato_transient", 'get_envato_themes', 10);
-//       //return $envato_themes;
-//     }
-// }
-// add_action('plugins_loaded' , 'sitecreator_check_transient')  ;
-
-function sitecreator_get_envato_themes() {
-  $url = "https://api.envato.com/v3/market/user/collection?id=8104310";
+function sitecreator_get_envato_themes($atts) {
+  $url = "https://api.envato.com/v3/market/user/collection?id=" . $atts['collection_id'];
   $curl = curl_init($url);
   
-  $personal_token = "ry8dHahFRPPKpRh804AXWvmvo5z7xCXa";
+  $personal_token = "";
   $header = array();
   $header[] = 'Authorization: Bearer '.$personal_token;
   $header[] = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0) Gecko/20100101 Firefox/41.0';
@@ -119,6 +107,34 @@ function sitecreator_get_envato_themes() {
       return;
     }
   }
+}
+
+function  sitecreator_get_envato_collections() {
+  $url = "https://api.envato.com/v3/market/user/collections";
+  $curl = curl_init($url);
+  
+  $personal_token = "ry8dHahFRPPKpRh804AXWvmvo5z7xCXa";
+  $header = array();
+  $header[] = 'Authorization: Bearer '.$personal_token;
+  $header[] = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0) Gecko/20100101 Firefox/41.0';
+  $header[] = 'timeout: 20';
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_HTTPHEADER,$header);
+  
+  $envatoRes = curl_exec($curl);
+  curl_close($curl);
+  $envatoRes = json_decode($envatoRes);
+
+  $collections = $envatoRes->collections;
+
+  foreach($collections as $collection) {
+    // sitecreator_get_envato_themes(array(
+    //   'collection_id' => $collections->id
+    // ));
+    echo $collection->id;
+  }
+
+
 }
 
 ?>
